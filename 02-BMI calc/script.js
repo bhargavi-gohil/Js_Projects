90f6f345f5e88d33625d90e82f20e089c9f69c52
bhargavi-gohil <bhargavigohil098@gmail.com>
2026-08-26 12:30:45 +0530

    button type and add form submission functionality

---
+++ 02-BMI calc/script.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    const h = parseInt(document.querySelector('#height').value)
+    const w = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML = "Please give a valid height"
+    } 
+    
+});

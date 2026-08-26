bcbbae0352565330396a59c7c07eeedbabe55d71
bhargavi-gohil <bhargavigohil098@gmail.com>
2026-08-26 11:52:34 +0530

    event listener on buttons

--- 01-Color-Changer/script.js
+++ 01-Color-Changer/script.js
@@ -2,8 +2,17 @@ let buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
 buttons.forEach(function (button){
-    buttons.addEventListener('click',function(e){
+    button.addEventListener('click',function(e){
         console.log(e);
-        console.log(e.target);
+        // console.log(e.target);
+        if(e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+        } else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        } else {
+            body.style.backgroundColor = 'yellow';
+        }
     })
-})
+});

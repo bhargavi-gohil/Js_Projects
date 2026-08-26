bd3dac0293e9fde2cce6494ae5c645ce5cd864d2
bhargavi-gohil <bhargavigohil098@gmail.com>
2026-08-26 13:19:12 +0530

    refactor form submission logic

--- 02-BMI calc/script.js
+++ 02-BMI calc/script.js
@@ -1,16 +1,31 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit',function(e){
-    e.preventDefault()
+// this use case will give you empty
+// const height = parseInt(document.querySelector('#height').value)
 
-    const h = parseInt(document.querySelector('#height').value)
-    const w = parseInt(document.querySelector('#weight').value)
-    const results = document.querySelector('#results')
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    if(height === '' || height < 0 || isNaN(height)){
-        results.innerHTML = `Please give a valid height ${height}`;
-    } 
-    if(weight === '' || weight < 0 || isNaN(weight)){
-        results.innerHTML = `Please give a valid height ${weight}`;
-    } 
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+
+    if(bmi < 18.6){
+        results.innerHTML = `BMI: ${bmi} Under Weight!`;
+    } else if(bmi >= 18.6 && bmi <= 24.9){
+        results.innerHTML = `BMI: ${bmi}  Normal Range!`;
+    } else {
+        results.innerHTML = `BMI: ${bmi} Overweight!`;
+    }
+}
 });
+
